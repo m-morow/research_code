09729a413700f6c82f1663bc0e6678ba654f3ae9
M. Morow Tan <97422443+m-morow@users.noreply.github.com>
2025-03-03 17:17:39 -0700

    ignore for now; testing

--- los_to_def.py
+++ los_to_def.py
@@ -5,7 +5,14 @@ import glob
 import json
 import shutil
 import numpy as np
+import matplotlib.pyplot as plt
 from cubbie.read_write_insar_utilities import isce_read_write
 
 los = '/Users/mata7085/Library/CloudStorage/OneDrive-UCB-O365/Data/IF_longterm/los_files/geo_los.rdr'
-xarray, yarray, data = isce_read_write.read_scalar_data(los)
+los_binary = np.fromfile(los)
+
+los_binary_matrix = np.reshape(los_binary, (9000, 10000))
+
+fig, ax = plt.subplots()
+ax.imshow(los_binary_matrix)
+plt.show()
