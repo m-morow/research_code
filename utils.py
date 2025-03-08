174a24acd561878ad732fac556814d28aaad3d6d
M. Morow Tan <97422443+m-morow@users.noreply.github.com>
2025-03-07 18:31:15 -0700

    add pandas df or csv file option

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ import numpy as np
 import glob
 from datetime import datetime
 
-def date_string_to_datetime(csv_file, header, src_col, target_col, sorted=True):
+def date_string_to_datetime(dataframe, header, src_col, target_col, sorted=True):
     """
     returns dataframe with converted datetime [YYYY-MM-DD] of YYYYMMDD string.
     compatible with matplotlib plotting x-axis 
@@ -15,7 +15,7 @@ def date_string_to_datetime(csv_file, header, src_col, target_col, sorted=True):
 
     Parameters:
     -------
-    csv_file: path to file
+    dataframe: pandas dataframe or path to .csv file
     header: line of header, usually 0 or None
     src_col: column that holds YYYYMMDD string
     target_col: column to update with datetime
@@ -26,7 +26,10 @@ def date_string_to_datetime(csv_file, header, src_col, target_col, sorted=True):
     sorted dataframe by date
     
     """
-    df = pd.read_csv(csv_file, header=header)
+    if '.csv' in dataframe: 
+        df = pd.read_csv(dataframe, header=header)
+    else:
+        df = dataframe
     for i, date in enumerate(np.array(df[src_col])):
         df.loc[[i], [target_col]] = datetime.strptime(str(date), "%Y%m%d")
     if sorted:
