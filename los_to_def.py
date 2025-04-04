96af207190eba9269cb8ca4d0820bfbb2a9abe58
M. Morow Tan <97422443+m-morow@users.noreply.github.com>
2025-04-04 14:50:38 -0600

    update imports and add enu2los function

--- los_to_def.py
+++ los_to_def.py
@@ -1,18 +1,50 @@
 #!/usr/bin/env python
 
-import os
-import glob
-import json
-import shutil
+import xarray as xr
 import numpy as np
 import matplotlib.pyplot as plt
 from cubbie.read_write_insar_utilities import isce_read_write
+from Tectonic_Utils.read_write import netcdf_read_write
+from Tectonic_Utils.geodesy import insar_vector_functions
+from cmcrameri import cm
 
-los = '/Users/mata7085/Library/CloudStorage/OneDrive-UCB-O365/Data/IF_longterm/los_files/geo_los.rdr'
-los_binary = np.fromfile(los)
+def enu2los(grd_e, grd_n, grd_u, flight_angle, incidence_angle, save=False, outpath=None):
+    """
+    Convert E, N, U .grd files to LOS using flight angle and incidence angle.
 
-los_binary_matrix = np.reshape(los_binary, (9000, 10000))
+    Parameters:
+    --------
+    grd_e
+    grd_n
+    grd_u
+    flight_angle: flight angle of satellite, accepts degree or radians, assuming angle in
+                  degree is always larger than 1
+    incidence_angle: average incidence angle of satellite, accepts degree or radians, assuming
+                     angle in degree is always larger than 1
+    save: default False, set if you want to save LOS xarray to .grd file
+    outpath: default None, set if save is True
 
-fig, ax = plt.subplots()
-ax.imshow(los_binary_matrix)
-plt.show()
+    Returns:
+    --------
+    los: xarray object
+    """
+    if flight_angle > 1:
+        flight_angle = np.deg2rad(flight_angle)
+        incidence_angle = np.deg2rad(incidence_angle)
+    
+    n = xr.open_dataset(grd_n)
+    e = xr.open_dataset(grd_e)
+    u = xr.open_dataset(grd_u)
+
+    los = insar_vector_functions.def3D_into_LOS(e, n, u, flight_angle, incidence_angle)
+
+    e.close()
+    n.close()
+    u.close()
+
+    if save:
+        netcdf_read_write.produce_output_netcdf(los['x'], los['y'], los['z'], 
+                                                zunits='los_m',                                 
+                                                netcdfname=outpath)
+
+    return los
