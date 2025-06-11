76df347273b2e0fa213eeaac421ef58675c3f53e
M. Morow Tan <97422443+m-morow@users.noreply.github.com>
2025-06-11 13:26:28 -0600

    initial commit

---
+++ pymc_driver.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from elastic_stresses_py import PyCoulomb
+from elastic_stresses_py.PyCoulomb import coulomb_collections as cc
+from elastic_stresses_py.PyCoulomb.fault_slip_triangle import triangle_okada
+from elastic_stresses_py.PyCoulomb.point_source_object import point_sources
+from elastic_stresses_py.PyCoulomb.disp_points_object.disp_points_object import Displacement_points
+from Tectonic_Utils.geodesy import fault_vector_functions
+from elastic_stresses_py.PyCoulomb import utilities, io_additionals, run_dc3d, run_mogi, conversion_math
+from elastic_stresses_py.PyCoulomb.inputs_object import io_intxt
+
+import arviz as az
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import xarray as xr
+import pytensor
+import pytensor.tensor as pt
+
+import pymc as pm
+
+from pymc import HalfCauchy, Model, Normal, sample
+from pytensor.graph import Apply, Op
