ddb4bffac7884323727c45d44a0d8cf49b85afc1
M. Morow Tan <97422443+m-morow@users.noreply.github.com>
2025-07-08 19:02:43 +0900

    update functions

--- pymc_driver.py
+++ pymc_driver.py
@@ -5,7 +5,6 @@ from elastic_stresses_py.PyCoulomb import coulomb_collections as cc
 from elastic_stresses_py.PyCoulomb.fault_slip_triangle import triangle_okada
 from elastic_stresses_py.PyCoulomb.point_source_object import point_sources
 from elastic_stresses_py.PyCoulomb.disp_points_object.disp_points_object import Displacement_points
-from Tectonic_Utils.geodesy import fault_vector_functions
 from elastic_stresses_py.PyCoulomb import utilities, io_additionals, run_dc3d, run_mogi, conversion_math
 from elastic_stresses_py.PyCoulomb.inputs_object import io_intxt
 
@@ -25,7 +24,7 @@ from pytensor.graph import Apply, Op
 import pymc_espy_utils
 
 def do_okada(params, slip, depth, dip):
-    inputs = pymc_espy_utils.do_update(inputs_orig, slip[0], depth[0], dip[0])
+    inputs = do_update(inputs_orig, slip[0], depth[0], dip[0])
 
     model_disp_points = run_dc3d.compute_ll_def(inputs, params, disp_points)
 
@@ -35,37 +34,68 @@ def do_okada(params, slip, depth, dip):
                                 source_object=inputs.source_object, receiver_object=inputs.receiver_object,
                                 receiver_normal=[], receiver_shear=[],
                                 receiver_coulomb=[], receiver_profile=[])
-    
-    dU = pymc_espy_utils.get_dU(MyOutObject.model_disp_points)
 
-    return dU
+    los = get_los(MyOutObject.model_disp_points)
 
-def my_loglike(slip, depth, dip, sigma, x, data):
-    for param in (slip, depth, dip, sigma, x, data):
+    return los
+
+def my_loglike(slip, depth, dip, sigma, data):
+    for param in (slip, depth, dip, sigma, data):
         if not isinstance(param, (float, np.ndarray)):
             raise TypeError(f"Invalid input type to loglike: {type(param)}") 
     model = do_okada(params, slip, depth, dip)
-    return -0.5 * ((data - model) / sigma) ** 2 - np.log(np.sqrt(2 * np.pi)) - np.log(sigma)
+    return -0.5 * ((data - model / sigma) ** 2 - np.log(np.sqrt(2 * np.pi)) - np.log(sigma))
 
 class LogLike(Op):
 
-    def make_node(self, slip, depth, dip, sigma, x, data):
+    def make_node(self, slip, depth, dip, sigma, data):
         slip = pt.as_tensor(slip)
         depth = pt.as_tensor(depth)
         dip = pt.as_tensor(dip)
         sigma = pt.as_tensor(sigma)
-        x = pt.as_tensor(x)
         data = pt.as_tensor(data)
 
-        inputs = [slip, depth, dip, sigma, x, data]
-        #outputs = [pt.vector()]
+        inputs = [slip, depth, dip, sigma, data]
         outputs = [data.type()]
 
         return Apply(self, inputs, outputs)
     
     def perform(self, node: Apply, inputs: list[np.ndarray], outputs: list[list[None]]) -> None:
-        slip, depth, dip, sigma, x, data = inputs
+        slip, depth, dip, sigma, data = inputs
 
-        loglike_eval = my_loglike(slip, depth, dip, sigma, x, data)
+        loglike_eval = my_loglike(slip, depth, dip, sigma, data)
 
         outputs[0][0] = np.asarray(loglike_eval)
+
+def custom_dist_loglike(data, slip, depth, dip, sigma, x):
+    # data, or observed is always passed as the first input of CustomDist
+    return LogLike(slip, depth, dip, sigma, x, data)
+
+if __name__ == "__main__":
+
+    #########################################################
+    # really important, set globals once before running !!  #
+    #########################################################
+    inputs_orig = read_intxt("normal_fault_in.txt")
+    params = PyCoulomb.configure_calc.configure_stress_calculation('my_config_normal.txt')
+    disp_points = io_additionals.read_disp_points("normal_fault_disp.txt")
+    data = np.loadtxt('los_data_2000.txt')
+    #########################################################
+    #########################################################
+
+    with pm.Model() as no_grad_model:
+        slip = pm.Normal("slip", mu=0.047, sigma=0.005) #mu=mean, sigma=st dev.
+        #depth = pm.Uniform("depth", lower=0.0, upper=1.0, initval=0.75)
+        depth = pm.TruncatedNormal("depth", mu=0.75, sigma=0.05, lower=0.65, upper=0.85)
+        #dip = pm.Normal("dip", mu=80, sigma=5)
+        dip = pm.TruncatedNormal("dip", mu=45, sigma=1, lower=30, upper=60, initval=45)
+        #dip = pm.Uniform("dip", lower=75.0, upper=90.0, initval=80)
+
+        # use a CustomDist with a custom logp function
+        likelihood = pm.CustomDist(
+            "likelihood", slip, depth, dip, sigma, observed=data, logp=custom_dist_loglike
+        )
+
+    with no_grad_model:
+        # Use custom number of draws to replace the HMC based defaults
+        idata_no_grad = pm.sample(3000, tune=1000)
