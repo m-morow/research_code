60da019e4c889de973ea83e496e29f932a2d95e8
M. Morow Tan <97422443+m-morow@users.noreply.github.com>
2025-07-08 20:25:19 +0900

    initial commit

---
+++ pymc_visualize.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from elastic_stresses_py import PyCoulomb
+from elastic_stresses_py.PyCoulomb import coulomb_collections as cc
+from elastic_stresses_py.PyCoulomb.fault_slip_triangle import triangle_okada
+from elastic_stresses_py.PyCoulomb.point_source_object import point_sources
+from elastic_stresses_py.PyCoulomb.disp_points_object.disp_points_object import Displacement_points
+from elastic_stresses_py.PyCoulomb import utilities, io_additionals, run_dc3d, run_mogi, conversion_math
+from elastic_stresses_py.PyCoulomb.inputs_object import io_intxt
+
+import arviz as az
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import xarray as xr
+import os
+
+import pytensor
+import pytensor.tensor as pt
+import pymc as pm
+from pymc import HalfCauchy, Model, Normal, sample
+from pytensor.graph import Apply, Op
+
+from pymc_espy_utils import get_los, read_intxt, do_update
+
+def plot_stats(pymc_model, round=3):
+    az.plot_trace(pymc_model)
+    az.plot_posterior(pymc_model)
+    az.summary(pymc_model, round_to=round)
+
+def plot_model()
